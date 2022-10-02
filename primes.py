f5aec56e5556eed719ec8472da41960c426e4e99
Brian Jalleh <brjalleh@gmail.com>
2022-10-02 13:56:23 +0100

    Test 1.1

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    
+    count = 2
+    
+    while len(list) < number_of_primes:
+        isPrime = True
+        
+        for i in range(2, count):
+            if(count%i == 0):
+                isPrime = False
+                break
+            
+        if(isPrime):
+            list.append(count)
+            
+        count += 1
+    
     return list
